211e1a791fadf15b9669c8a569777797de08a92c
Mauro Minella <mauro.minella@microsoft.com>
2023-08-09 11:55:13 +0200

    First commit for PythonWebApp

--- app.py
+++ app.py
@@ -11,12 +11,13 @@ app = Flask(__name__)
 # the associated function.
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
-def hello_world(name="anonymous"):
-    return f'Hello World from {name}'
- 
+def hello_world():
+    return f'Hello World from Mauro Minella'
+
+    
 # main driver function
 if __name__ == '__main__':
  
     # run() method of Flask class runs the application
     # on the local development server.
-    app.run("Mauro Minella")
+    app.run()
