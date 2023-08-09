05c230295e90368df1e20360667aa63e56bebafe
Mauro Minella <mauro.minella@microsoft.com>
2023-08-09 11:55:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ def hello_world():
 
     
 # main driver function
-if __name__ == '__main__':
- 
+if __name__ == '__main__': 
     # run() method of Flask class runs the application
     # on the local development server.
     app.run()
